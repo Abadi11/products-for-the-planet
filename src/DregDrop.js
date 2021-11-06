b454213df41891a99b031029251ad07060435d6f
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2021-11-06 23:31:10 +0000

    update CSS

--- src/DregDrop.js
+++ src/DregDrop.js
@@ -8,6 +8,7 @@ const cardsFromBackend = [
     { id: "card-2", content: "BBBBBB" },
   ],
   [{ id: "card-3", content: "CCCCC" }],
+  [{id: "card-4", content: ""}]
 ];
 
 function DregDrop() {
@@ -26,11 +27,11 @@ function DregDrop() {
     const [removed] = oldColumn.splice(source.index, 1);
     const newColumn = cards[newColumnIndex];
     newColumn.splice(destination.index, 0, removed);
-    //setCards([...cards]);
+    //setCards([...cards, newColumn]);
   }
 
   return (
-    <div style={{ display: "flex", justifyContent: "center", height: "100%" }}>
+    <div style={{ display: "flex", justifyContent: "center", height: "100vh", width: '100%' }}>
       <DragDropContext
         onDragEnd={(result) => {
           return console.log(result), onDragEnd(result);
@@ -38,49 +39,56 @@ function DregDrop() {
       >
         {cards.map((column, columnIndex) => {
           return (
-            <Droppable key={columnIndex} droppableId={columnIndex.toString()}>
-              {(provided, snapshot) => {
-                return (
-                  <div
-                    {...provided.droppableProps}
-                    ref={provided.innerRef}
-                    style={{
-                      background: snapshot.isDraggingOver
-                        ? "lightblue"
-                        : "lightgrey",
-                      padding: 4,
-                      width: 250,
-                      minHeight: 500,
-                    }}
-                  >
-                    {cards[columnIndex].map((card, index) => {
-                      return (
-                        // key and draggable can be depended on id
-                        <Draggable
-                          key={card.id}
-                          draggableId={card.id}
-                          index={index}
-                        >
-                          {(provided, snapshot) => {
-                            return (
-                              <div
-                                ref={provided.innerRef}
-                                {...provided.draggableProps}
-                                {...provided.dragHandleProps}
-                              >
-                                <FakeCard id={card.id} content={card.content} />
-                              </div>
-                            );
-                          }}
-                        </Draggable>
-                      );
-                    })}
-                  </div>
-                );
-              }}
-            </Droppable>
+            <div style={{margin: 10}}>
+              <Droppable key={columnIndex} droppableId={columnIndex.toString()}>
+                {(provided, snapshot) => {
+                  return (
+                    <div
+                      {...provided.droppableProps}
+                      ref={provided.innerRef}
+                      style={{
+                        background: snapshot.isDraggingOver
+                          ? "lightblue"
+                          : "lightgrey",
+                        padding: 4,
+                        width: '250',
+                        height: '500',
+                      }}
+                    >
+                      {cards[columnIndex].map((card, index) => {
+                        return (
+                          // key and draggable can be depended on id
+                          <Draggable
+                            key={card.id}
+                            draggableId={card.id}
+                            index={index}
+                          >
+                            {(provided, snapshot) => {
+                              return (
+                                <div
+                                  ref={provided.innerRef}
+                                  {...provided.draggableProps}
+                                  {...provided.dragHandleProps}
+                                >
+                                  <FakeCard
+                                    id={card.id}
+                                    content={card.content}
+                                  />
+                                </div>
+                              );
+                            }}
+                          </Draggable>
+                        );
+                      })}
+                    </div>
+                  );
+                }}
+              </Droppable>
+            </div>
           );
         })}
+        {/*  */}
+        
       </DragDropContext>
     </div>
   );
