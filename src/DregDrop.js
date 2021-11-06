c7d31015a2cffc5812516f1f18455704bf77a15a
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2021-11-06 20:42:57 +0000

    update drag-drop draft

--- src/DregDrop.js
+++ src/DregDrop.js
@@ -1,19 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import FakeCard from "./FakeCard";
 
-const columns = [1, 2];
-const cardsFromBackend = [<FakeCard />];
-// for (let i=0; i<=5; i++){
-//  cardsFromBackend.push(<FakeCard />)
-// }
+const cardsFromBackend = [
+  [
+    { id: "card-1", content: "asdads" },
+    { id: "card-2", content: "BBBBBB" },
+  ],
+  [{ id: "card-3", content: "CCCCC" }],
+];
+
 function DregDrop() {
+  const [cards, setCards] = useState(cardsFromBackend);
+  function onDragEnd(result) {
+    console.log("columns: ", result);
+    if (!result.destination) return;
+    const oldColumnIndex = result.source.droppableId;
+    const newColumnIndex = result.destination.droppableId;
+    const draggedCardId = result.draggableId;
+    // console.log({ newColumnIndex, draggedCardId });
+    const { source, destination } = result;
+    const oldColumn = cards[oldColumnIndex];
+    // console.log("column", column);
+    // const copiedItems = [...newColumn];
+    const [removed] = oldColumn.splice(source.index, 1);
+    const newColumn = cards[newColumnIndex];
+    newColumn.splice(destination.index, 0, removed);
+    //setCards([...cards]);
+  }
+
   return (
     <div style={{ display: "flex", justifyContent: "center", height: "100%" }}>
-      <DragDropContext onDragEnd={(result) => console.log(result)}>
-        {columns.map((column, id) => {
+      <DragDropContext
+        onDragEnd={(result) => {
+          return console.log(result), onDragEnd(result);
+        }}
+      >
+        {cards.map((column, columnIndex) => {
           return (
-            <Droppable droppableId={id}>
+            <Droppable key={columnIndex} droppableId={columnIndex.toString()}>
               {(provided, snapshot) => {
                 return (
                   <div
@@ -28,24 +53,27 @@ function DregDrop() {
                       minHeight: 500,
                     }}
                   >
-                    {cardsFromBackend.map((card, index) => {
-                     return (
-                      // key and draggable can be depended on id
-                      <Draggable key={index} draggableId ={index} index={index}>
-                       {(provided, snapshot) => {
-                        return (
-                         <div ref={provided.innerRef}
-                         {...provided.draggableProps}
-                         {...provided.dragHandleProps}
-                         style={{
-                          ...provided.draggableProps.style
-                         }}>
-                          {card}
-                         </div>
-                        )
-                       }}
-                      </Draggable>
-                     )
+                    {cards[columnIndex].map((card, index) => {
+                      return (
+                        // key and draggable can be depended on id
+                        <Draggable
+                          key={card.id}
+                          draggableId={card.id}
+                          index={index}
+                        >
+                          {(provided, snapshot) => {
+                            return (
+                              <div
+                                ref={provided.innerRef}
+                                {...provided.draggableProps}
+                                {...provided.dragHandleProps}
+                              >
+                                <FakeCard id={card.id} content={card.content} />
+                              </div>
+                            );
+                          }}
+                        </Draggable>
+                      );
                     })}
                   </div>
                 );
--- src/FakeCard.js
+++ src/FakeCard.js
@@ -1,13 +1,25 @@
-import { flexbox } from '@chakra-ui/styled-system'
-import React from 'react'
+import { flexbox } from "@chakra-ui/styled-system";
+import React from "react";
 
-function FakeCard() {
- //const id = []
- return (
-  <div style={{display: 'flex', userSelect: 'none', padding: 16, margin: '0 0 8px 0', minHeight: '50px', backgroundColor: 'black', color: 'green'}}>
-   <p>1</p>
-  </div>
- )
+function FakeCard({ id, content }) {
+  //const id = []
+  return (
+    <div
+      style={{
+        display: "flex",
+        userSelect: "none",
+        padding: 16,
+        margin: "0 0 8px 0",
+        minHeight: "50px",
+        backgroundColor: "black",
+        color: "green",
+      }}
+    >
+      <h3>Id: {id}</h3>
+
+      <p>{content}</p>
+    </div>
+  );
 }
 
-export default FakeCard
+export default FakeCard;
